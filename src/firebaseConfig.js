2a8550536cc61c7cf673098e10636edd229a0d82
Juan Garcia <garciaperezjuandedios2001@gmail.com>
2023-11-15 23:13:01 -0600

    Auth seccion 8

---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDvwRZxUvXFkRGeFnqTJynB0aa3uoqTj-o",
+  authDomain: "vue-3-2023-70808.firebaseapp.com",
+  projectId: "vue-3-2023-70808",
+  storageBucket: "vue-3-2023-70808.appspot.com",
+  messagingSenderId: "897106472616",
+  appId: "1:897106472616:web:b3d599649416d849f09c9d"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+export { auth };
---
+++ src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <form @submit.prevent="handlesubmit">
+      <input type="email" placeholder="Ingrese email" v-model.trim="email">
+      <input type="password" placeholder="Ingrese contraseña" v-model.trim="password">
+      <button type="submit" :disabled="userStore.loadingUser">Acceso</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import{useUserStore} from '../stores/user';
+//import {useRouter} from 'vue-router';
+
+const userStore = useUserStore();
+//const router = useRouter();
+
+const email = ref('juan3@test.com')
+const password = ref('123123')
+
+const handlesubmit = async() => {
+  if(!email.value || password.value.length < 6){
+    return alert('Llena los campos')
+  }
+  await userStore.loginUser(email.value, password.value)
+  //router.push('/')
+}
+
+</script>
